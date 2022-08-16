508a521a361d5bcf73b98138d6f68e9823fe67d4
Valera97531 <10002000valera@gmail.com>
2022-08-16 21:23:36 +0300

    made two responsible screen

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,24 @@
 import * as flsFunctions from "./modules/function.js";
 flsFunctions.isWebp();
+
+document.addEventListener('DOMContentLoaded', () => {
+	const menu = document.querySelector('.header__menu'),
+		burger = document.querySelector('.header__burger'),
+		menuClose = document.querySelector('.header__menu-close'),
+		menuLink = document.querySelectorAll('.header__link');
+
+	burger.addEventListener('click', () => {
+		menu.classList.toggle('_active');
+	});
+
+	menuClose.addEventListener('click', () => {
+		menu.classList.remove('_active');
+	});
+
+	menuLink.forEach(item => {
+		item.addEventListener('click', () => {
+			menu.classList.remove('_active');
+		});
+	});
+
+});
